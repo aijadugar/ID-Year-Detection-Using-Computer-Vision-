1b561a8f247c5594919ee2ccc90b67ca8f675bd3
BARI ANKIT VINOD <139578960+OnlyCR7@users.noreply.github.com>
2025-09-10 03:07:44 +0530

    updated settings.py to hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,6 +59,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://id-year-detection-using-computer-vi.vercel.app/"
 ]
 
+ALOOWED_HOSTS = ["https://id-year-detection-using-computer-vision.onrender.com"]
+
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
